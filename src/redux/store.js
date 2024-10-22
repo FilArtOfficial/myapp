53a13a9b2f9edf79c2cceb5f0cbb130a06133067
FilArtOfficial <118174060+FilArtOfficial@users.noreply.github.com>
2024-10-22 18:27:49 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,3 @@
-// redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './slices/productsSlice';
 import categoriesReducer from './slices/categoriesSlice';
